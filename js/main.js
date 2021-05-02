a92bc9d694227397b0576c1e78d2d6f4a4831494
Anderson Medeiros <anderson.santos.medeiros@hotmail.com>
2021-05-01 23:49:32 -0300

    Added basic CSS

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('fakeTelegram', ['ngRoute']);
+var app = angular.module('fakegram', ['ngRoute']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
